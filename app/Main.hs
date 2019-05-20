8c1af4e049b1a63fe7255e49103464daa29ef948
kvnflm <kvnflm@gmail.com>
2019-05-20 22:04:00 +0900

    Add authentication

--- app/Main.hs
+++ app/Main.hs
@@ -7,32 +7,51 @@ import qualified GitHub.Endpoints.PullRequests as PR
 import qualified GitHub.Endpoints.Issues as Issues
 import qualified GitHub.Data.Options as Options
 import qualified GitHub.Request as R
+import qualified GitHub.Data.Definitions as Definitions
 import GitHub.Auth
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except
+import Data.Maybe (isNothing)
 import Data.Text (Text)
-import Data.Vector (Vector)
+import Data.Vector (Vector, filter)
+import Data.Foldable (traverse_)
 import System.ReadEnvVar
 import Lib
 
-getToken :: ExceptT String IO Auth
-getToken = ExceptT $ do
-  maybeToken <- lookupEnv "GITHUB_TOKEN"
-  return $ fmap OAuth (maybeToEither "Not found" maybeToken)
+data MyError = NoTokenFound | GitHubError Definitions.Error deriving Show
 
-getData :: Text -> Text -> ExceptT PR.Error IO (Vector PR.SimplePullRequest, Vector Issues.Issue)
-getData owner repoName = do
-  let prRequest = PR.pullRequestsForR (PR.mkOwnerName owner) (PR.mkRepoName repoName) mempty PR.FetchAll
-  pullRequests <- ExceptT $ R.executeRequest' prRequest
-  let issueRequest = Issues.issuesForRepoR　(Issues.mkOwnerName owner) (Issues.mkRepoName repoName) mempty Issues.FetchAll
-  issues <- ExceptT $ R.executeRequest' issueRequest
-  return (pullRequests, issues)
+data AllData = AllData (Vector PR.SimplePullRequest) (Vector Issues.Issue)
+
+lookupToken :: ExceptT MyError IO Auth
+lookupToken = ExceptT $ do
+  maybeToken <- lookupEnv "GITHUB_TOKEN"
+  return $ fmap OAuth (maybeToEither NoTokenFound maybeToken)
+
+getData' :: Auth -> Text -> Text -> ExceptT MyError IO AllData
+getData' auth owner repoName = do
+  let prRequest = PR.pullRequestsForR (PR.mkOwnerName owner) (PR.mkRepoName repoName) Options.stateAll PR.FetchAll
+  pullRequests <- withExceptT errorMapper $ ExceptT $ R.executeRequest auth prRequest
+  let issueRequest = Issues.issuesForRepoR　(Issues.mkOwnerName owner) (Issues.mkRepoName repoName) Options.stateAll Issues.FetchAll
+  issues <- withExceptT errorMapper $ ExceptT $ R.executeRequest auth issueRequest
+  return $ AllData pullRequests (filterOutPullRequests issues)
+  where
+    errorMapper :: Definitions.Error -> MyError
+    errorMapper e = GitHubError e
+
+getData :: ExceptT MyError IO AllData
+getData = do
+  auth <- lookupToken
+  getData' auth "octocat" "hello-world"
+
+filterOutPullRequests :: Vector Issues.Issue -> Vector Issues.Issue
+filterOutPullRequests issues = Data.Vector.filter (isNothing . Issues.issuePullRequest) issues
 
 main :: IO ()
 main = do
-  result <- runExceptT $ getData "octocat" "hello-world"
+  result <- runExceptT getData
   case result of
-    Left error ->
-      putStrLn (show error)
-    Right (prs, issues) ->
+    Left error -> putStrLn (show error)
+    Right (AllData prs issues) -> do
       putStrLn $ "Got " ++ (show (length prs)) ++ " pull requests and " ++ (show (length issues)) ++ " issues."
+      traverse_ (putStrLn . show) titles
+      where titles :: Vector Text = fmap Issues.issueTitle issues
