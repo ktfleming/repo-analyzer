efe165a3b56149cc8ac1307d7e87dfdaec189ccb
kvnflm <kvnflm@gmail.com>
2019-05-20 22:50:32 +0900

    Add getPRHistogram

--- app/Main.hs
+++ app/Main.hs
@@ -12,16 +12,20 @@ import GitHub.Data.Definitions
 import GitHub.Auth
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Except
-import Data.Maybe (isNothing)
+import Data.Maybe (isNothing, fromMaybe)
 import Data.Text (Text)
 import Data.Vector (Vector, filter)
 import Data.Foldable (traverse_)
+import Data.Time
+import Data.Time.Calendar
+import qualified Data.Map.Strict as Map
 import System.ReadEnvVar
 import Lib
 
 data MyError = NoTokenFound | GitHubError Error deriving Show
 
 data AllData = AllData (Vector SimplePullRequest) (Vector Issue)
+type DayHistogram = Map.Map Day Int
 
 lookupToken :: ExceptT MyError IO Auth
 lookupToken = ExceptT $ do
@@ -47,6 +51,23 @@ getData = do
 filterOutPullRequests :: Vector Issue -> Vector Issue
 filterOutPullRequests issues = Data.Vector.filter (isNothing . issuePullRequest) issues
 
+getPRHistogram :: Day -> Vector SimplePullRequest -> DayHistogram
+getPRHistogram today prs =
+  foldr foldfn Map.empty prs
+    where
+      foldfn :: SimplePullRequest -> DayHistogram -> DayHistogram
+      foldfn pr hist =
+        let openDay :: Day = utctDay (simplePullRequestCreatedAt pr)
+            closedDay :: Day = fromMaybe today $ fmap utctDay (simplePullRequestClosedAt pr)
+            days :: [Day] = enumFromThen openDay closedDay
+            subfold :: Day -> DayHistogram -> DayHistogram
+            subfold day h = Map.alter updater day h
+              where
+                updater :: Maybe Int -> Maybe Int
+                updater Nothing = Just 1
+                updater (Just x) = Just (x + 1)
+        in foldr subfold hist days
+  
 main :: IO ()
 main = do
   result <- runExceptT getData
